206af76b25506e6600ff05205ec393db91a4e7a7
jarrett-m <jarrettminton00@gmail.com>
2023-09-15 02:05:28 +0000

    looks good! victim program

--- victim/rsa_victim/src/main.rs
+++ victim/rsa_victim/src/main.rs
@@ -1,6 +1,7 @@
 use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
 use rand;
-
+use rsa::traits::PrivateKeyParts;
+use std::thread::sleep;
 fn main() {
     // Generate an RSA key pair
     let mut rng = rand::thread_rng();
@@ -11,9 +12,15 @@ fn main() {
     // Encrypt
     let data = b"secret password";
     let enc_data = pub_key.encrypt(&mut rng, Pkcs1v15Encrypt, &data[..]).expect("failed to encrypt");
-    
+    println!("Starting!");
+
     // Decrypt
-    let dec_data = priv_key.decrypt(Pkcs1v15Encrypt, &enc_data).expect("failed to decrypt");
+    let mut dec_data = priv_key.decrypt(Pkcs1v15Encrypt, &enc_data).expect("failed to decrypt");
+    for _ in 0..5{
+        dec_data = priv_key.decrypt(Pkcs1v15Encrypt, &enc_data).expect("failed to decrypt");
+    }
     println!("Done!");
     println!("{}", String::from_utf8(dec_data).unwrap());
+    println!("Secret key: {:b}", priv_key.d())
 }
+//time when each thingy starts
---
+++ victim/rsa_victim/target/debug/deps/rsa_victim-0d5e0680e3e8eb9c.d
@@ -0,0 +1,5 @@
+/home/sca-example/victim/rsa_victim/target/debug/deps/rsa_victim-0d5e0680e3e8eb9c: src/main.rs
+
+/home/sca-example/victim/rsa_victim/target/debug/deps/rsa_victim-0d5e0680e3e8eb9c.d: src/main.rs
+
+src/main.rs:
